c28d28c2604225993af5f0d7e39fd3ee17dae975
herothezero90 <karl.a.lenard@gmail.co>
2024-06-29 16:00:25 +0200

    fixed anchor issues

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
+function scrollToSection(id) {
+  const element = document.getElementById(id);
+  if (element) {
+      element.scrollIntoView({ behavior: 'smooth' })
+      console.log ("scrollToSection");
+  }
+}
 document.addEventListener('DOMContentLoaded', () => {
-    const flexItems = document.querySelectorAll('#flex1 .flex');
-    flexItems.forEach((item, index) => {
-        item.style.setProperty('--animation-delay', `${index * 0.2}s`); // Adjust the delay as needed
-    });
+  const flexItems = document.querySelectorAll('#flex1 .flex');
+  flexItems.forEach((item, index) => {
+      item.style.setProperty('--animation-delay', `${index * 0.2}s`); 
+  });
 });
-
